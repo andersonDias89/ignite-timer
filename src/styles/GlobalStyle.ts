ca6b101f60e2b2be4fa6696de622ecf08aebee64
Anderson Dias <andersondiasmd25@gmail.com>
2022-07-12 16:25:09 -0300

    feat: adicionando cores e estilos globais

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -5,5 +5,14 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    outline: 0;
+}
+
+body {
+    background-color: ${props => props.theme['gray-900']};
+    color: ${props => props.theme['gray-100']};
+    font-family: 'Roboto', sans-serif;
+
+    --webkit-font-smoothing: antialiased;
 }
 `
