2c4cb019930b922b5a6f9288d5e38123cf11163a
Anderson Dias <andersondiasmd25@gmail.com>
2022-07-12 21:49:07 -0300

    refactor: criando a index do Header e o styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <div>Logo</div>
+      <div>
+        <span>Timer</span>
+        <span>App</span>
+      </div>
+    </HeaderContainer>
+  )
+}
